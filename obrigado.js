8eaf46bc64cea9b963417e27e1a781724fd23370
Walter <waltersantosfilho94@gmail.com>
2026-06-07 18:08:43 -0300

    Adiciona novos projetos e melhorias no layout

---
+++ obrigado.js
@@ -0,0 +1,41 @@
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 50
+        },
+        color: {
+            value: "#00d2ff"
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.3
+        },
+        size: {
+            value: 3
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#00d2ff",
+            opacity: 0.2,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2
+        }
+    },
+
+    interactivity: {
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
+            }
+        }
+    },
+
+    retina_detect: true
+});
